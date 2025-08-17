f1d814fe626d4640f3940d8e102ca76bb3e9c423
unknown <huangetienne123@gmail.com>
2025-08-18 01:06:26 +0200

    ajout axios token

---
+++ ICC_Calendar/frontend/src/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+});
+
+api.interceptors.request.use(
+    //check s'il y a un token d'accès dans le localStorage
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
