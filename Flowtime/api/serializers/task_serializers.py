050a139af07c1baa9fc7e76b970e8b53796d46c5
unknown <huangetienne123@gmail.com>
2025-12-03 19:10:00 +0100

    ICC-24 : Ajout du serializers pour les Tasks

---
+++ Flowtime/api/serializers/task_serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from ..models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'content', 'is_completed', 'created_at', 'updated_at', 'start_time', 'end_time', 'owner', 'time_spent', 'order']
+        
+        # Pour ne pas modifier ces champs via l'API
+        read_only_fields = ['created_at', 'updated_at', 'owner']
