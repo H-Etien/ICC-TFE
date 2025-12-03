de7dfa5bd667172ef2eb45f7e881336795bc8d61
unknown <huangetienne123@gmail.com>
2025-12-03 19:23:50 +0100

    ICC-23 : Ajout de serializers pour les Task

---
+++ Flowtime/api/views/task_views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+
+from ..serializers import TaskSerializer
+from ..models import Task
+
+class TaskListCreate(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+    
+    # Pour ne retourner que les tâches de l'utilisateur connecté
+    def get_queryset(self):
+        user = self.request.user
+        return Task.objects.filter(owner=user).order_by('order')
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
