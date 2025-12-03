793099cf950124f4be188beac21a85c87cee7a24
unknown <huangetienne123@gmail.com>
2025-12-03 03:39:50 +0100

    ICC-4 : Installation de React + Axios

--- backend.sh
+++ backend.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Activer le backend Django
 
 source venv/Scripts/activate
---
+++ frondend.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Activer le frontend React
+
+source venv/Scripts/activate
+echo "venv activé."
+
+cd Flowtime
+cd frontend
+echo "frontend activé."
+npm run dev
