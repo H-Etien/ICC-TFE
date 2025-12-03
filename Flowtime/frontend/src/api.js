49cb577e6b27a9f258fca5cb84cd89a8e0578767
unknown <huangetienne123@gmail.com>
2025-12-03 23:29:57 +0100

    ICC-6 : Ajout des JWT Token d'accès pour l'api

---
+++ Flowtime/frontend/src/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+});
+
+// Intercepteur pour ajouter le token d'accès à chaque requête
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
