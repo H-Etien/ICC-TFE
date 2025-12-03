a316e9e574d0376eb9fee0b5ff60788ada6758f2
unknown <huangetienne123@gmail.com>
2025-12-03 18:33:55 +0100

    ICC-7 : Ajout des liens pour accéder aux tokens

--- Flowtime/backend/urls.py
+++ Flowtime/backend/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# Pour l'enregistrement des utilisateurs
+from api.views import UserCreateView 
+# Pour les tokens JWT
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    path('api/user/register/', UserCreateView.as_view(), name='user_create'),
+    
+    path("api/auth/", include("rest_framework.urls"))
 ]
