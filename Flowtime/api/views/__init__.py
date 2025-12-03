1b38c535c7c5fb8cb1c8ba9aae7f399488fb9154
unknown <huangetienne123@gmail.com>
2025-12-03 23:16:15 +0100

    ICC-26 : Changement dans l'url pour route Project

--- Flowtime/api/views/__init__.py
+++ Flowtime/api/views/__init__.py
@@ -1,3 +1,3 @@
 from .user_views import UserCreateView
 from .task_views import TaskListCreateView
-from .project_views import ProjectListCreateView
+from .project_views import ProjectListCreateView, ProjectDetailView
--- Flowtime/backend/urls.py
+++ Flowtime/backend/urls.py
@@ -17,18 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# Pour l'enregistrement des utilisateurs
-from api.views import UserCreateView 
-# Pour les tokens JWT
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    path('api/user/register/', UserCreateView.as_view(), name='user_create'),
-    
-    path("api/auth/", include("rest_framework.urls"))
+    path("api/auth/", include("rest_framework.urls")),
+    path('api/', include('api.urls')),
 ]
