1a088962a09d011d6868b87fd00669a2b1000cb9
unknown <huangetienne123@gmail.com>
2025-12-03 23:49:22 +0100

    ICC-7 : Ajout JWT Token et protected Route

---
+++ Flowtime/frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,63 @@
+import { Navigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import { useState, useEffect } from "react";
+
+import api from "../api";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+
+// Pour protéger les routes nécessitant une authentification
+function ProtectedRoute({ children }) {
+    const [isAuthorized, setIsAuthorized] = useState(null);
+
+    useEffect(() => {
+        authenticate().catch(() => setIsAuthorized(false)); // si problème, pas autorisé à se connecter sur le lien
+    }, []);
+
+    const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        try {
+            const response = await api.post("/api/token/refresh/", {
+                refresh: refreshToken,
+            });
+
+            // Pour mettre à jour le token d'accès dans le localStorage
+            if (response.status === 200) {
+                localStorage.setItem(ACCESS_TOKEN, response.data.access);
+                setIsAuthorized(true);
+            } else {
+                setIsAuthorized(false);
+            }
+        } catch (error) {
+            console.error("Token refresh Error :", error);
+            setIsAuthorized(false);
+        }
+    };
+
+    const authenticate = async () => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (!token) {
+            setIsAuthorized(false);
+            return;
+        }
+
+        // Pour vérifier si le token a expiré
+        const decoded = jwtDecode(token);
+        const TokenExpiration = decoded.exp * 1000;
+        const now = Date.now() / 1000;
+
+        // Rafraichir le token s'il a expiré
+        if (TokenExpiration < now) {
+            await refreshToken();
+        } else {
+            // Token pas encore expiré
+            setIsAuthorized(true);
+        }
+    };
+
+    if (isAuthorized === null) {
+        return null;
+    }
+    return isAuthorized ? children : <Navigate to="/login" />;
+}
+
+export default ProtectedRoute;
